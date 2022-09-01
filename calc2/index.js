92c0aece8682723983881950bfe1dcb070e0345f
santgonnzalez <santiago.gonnzalez1@gmail.com>
2022-09-01 20:58:50 -0300

    Finished calc simple

--- calc2/index.js
+++ calc2/index.js
@@ -1,4 +1,36 @@
 const txtOp1 = document.getElementById('op1');
-const txtOperador = document.getElementById('operador');
+const txtOperacion = document.getElementById('operacion');
 const txtOp2 = document.getElementById('op2');
-const txtCalcular = document.getElementById('calcular');
+const botonCalcular = document.getElementById('calcular');
+const pResult = document.getElementById('resultado') ;
+
+
+botonCalcular.addEventListener('click', calcular)
+
+function calcular(){
+    const operacion = txtOperacion.value;
+    const op1 = parseFloat(txtOp1.value);
+    const op2 = parseFloat(txtOp2.value);
+
+
+
+    if((operacion == "+" || operacion == "-" || operacion == '*' ||
+    operacion == "/") && !isNaN(op1) && !isNaN(op2)){
+        let resultado;
+        switch(operacion){
+            case '+': resultado = op1 + op2;
+            break;
+            case '-': resultado = op1 - op2;
+            break;
+            case '*': resultado = op1 * op2;
+            break;
+            case '/': resultado = op1 / op2;
+            break;
+        }
+        pResult.style = 'color:blue'
+        pResult.innerText = "= " + resultado;
+    }else{
+        pResult.style = 'color:red'
+        pResult.innerText= "Calculo imposible";
+    }
+}
