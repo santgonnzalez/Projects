81643542e51dbfa21a4e56673e2c61a53dfabf03
santgonnzalez <santiago.gonnzalez1@gmail.com>
2022-09-01 20:19:44 -0300

    first steps

---
+++ calc2/index.js
@@ -0,0 +1,4 @@
+const txtOp1 = document.getElementById('op1');
+const txtOperador = document.getElementById('operador');
+const txtOp2 = document.getElementById('op2');
+const txtCalcular = document.getElementById('calcular');
