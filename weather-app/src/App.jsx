c18cf7b85b47fc3a0429c3c53e6499dd0d9fe121
Santiago Gonzalez <santiago.gonnzalez1@gmail.com>
2024-05-20 13:37:49 -0300

    use state and const created for api key and for url

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 import { Axios } from 'axios'
 
 function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState("")
+
+  const API_KEY = "26e24f1309fc6ae7b52e32aececbe5f4"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+
+  const searchLocation = (event) => {
+    if(event.key === "Enter"){
+      axios.get(url)
+      .then((response) => {
+        setData(response-data)
+        console.log(response.data);
+      })
+      setLocation("")
+    }
+  }
+
   return (
-    <>
+    <> 
       <div className=' w-full h-full relative'>
         <div className=' text-center p-4'>
-          <input type="text" className=' py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/10 shadow-md' />
+          <input type="text" className=' py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/10 shadow-md' 
+          placeholder='Enter location..' 
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyDownCapture={searchLocation}/>
         </div>
       </div>
     </>
