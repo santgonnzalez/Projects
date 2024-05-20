d628bc96c23fc7893dc22eb8a71a8b967f95c2de
Santiago Gonzalez <santiago.gonnzalez1@gmail.com>
2024-05-20 13:09:18 -0300

    input created and first styles

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import './App.css'
+import { Axios } from 'axios'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <div className=' w-full h-full relative'>
+        <div className=' text-center p-4'>
+          <input type="text" className=' py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/10 shadow-md' />
+        </div>
+      </div>
     </>
   )
 }
